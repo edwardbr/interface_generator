568ff7272f95628e1e87e33574206c00f1962c1c
Edward Boggis-Rolfe <edward@secretarium.com>
2022-01-20 14:41:49 +0000

    Migrated old generators into this repo

---
+++ edl_parser/flatbuffer_gen.h
@@ -0,0 +1,5 @@
+namespace flatbuffer
+{
+	//entry point
+	void writeFiles(const ClassObject& m_ob, std::filesystem::path& path, std::vector<std::string>& namespaces/*, const std::string& header*/);
+}
